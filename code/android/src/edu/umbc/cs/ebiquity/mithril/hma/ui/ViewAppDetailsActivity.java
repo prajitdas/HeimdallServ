3f797d9bc623749d0b76524de6adab6d155a58aa
Prajit Kumar Das <prajitdas@gmail.com>
2015-09-11 18:31:32 -0400

    feedback points started

--- code/android/src/edu/umbc/cs/ebiquity/mithril/hma/ui/ViewAppDetailsActivity.java
+++ code/android/src/edu/umbc/cs/ebiquity/mithril/hma/ui/ViewAppDetailsActivity.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PermissionInfo;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import edu.umbc.cs.ebiquity.mithril.hma.R;
 import edu.umbc.cs.ebiquity.mithril.hma.util.AppPermListAdapter;
 import edu.umbc.cs.ebiquity.mithril.hma.util.HMADBHelper;
@@ -17,6 +22,10 @@ public class ViewAppDetailsActivity extends ListActivity {
 	private PackageManager packageManager;
 	private List<PermissionInfo> appPermList;
 	private AppPermListAdapter appPermListAdapter;
+	private ImageButton mImgBtnLaunchApp;
+	private ImageButton mImgBtnAppIsGood;
+	private ImageButton mImgBtnAppIsBad;
+	private String packageName; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,21 +42,74 @@ public class ViewAppDetailsActivity extends ListActivity {
 	
 	private void initView() {
 		packageManager = getApplicationContext().getPackageManager();
+		
+		mImgBtnLaunchApp = (ImageButton) findViewById(R.id.launch_app_btn);
+		mImgBtnAppIsGood = (ImageButton) findViewById(R.id.app_is_good_btn);
+		mImgBtnAppIsBad = (ImageButton) findViewById(R.id.app_is_bad_btn);
 
-		String appPkgName = getIntent().getStringExtra(HMADBHelper.getAppPackageName());
+		packageName = getIntent().getStringExtra(HMADBHelper.getAppPackageName());
 		HMADBHelper hmaDBHelper = new HMADBHelper(this);
 		SQLiteDatabase hmaDB = hmaDBHelper.getWritableDatabase();
-		setTitle(getResources().getText(R.string.title_activity_view_app_details)+": "+hmaDBHelper.readAppNameByPackageName(hmaDB, appPkgName));
+		setTitle(getResources().getText(R.string.title_activity_view_app_details)+": "+hmaDBHelper.readAppNameByPackageName(hmaDB, packageName));
 		
-		setAppPermList(appPkgName);
+		setAppPermList(packageName);
 		
 		appPermListAdapter = new AppPermListAdapter(ViewAppDetailsActivity.this, R.layout.app_list_item, getAppPermList());
 		// Bind to new adapter. 
 		if(appPermListAdapter.getCount() == 0)
 			appPermListAdapter = null;
 		setListAdapter(appPermListAdapter);
+		
+		setOnClickListeners();
 	}
 	
+	private void setOnClickListeners() {
+		mImgBtnLaunchApp.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				try { 
+					Intent intent = packageManager.getLaunchIntentForPackage(packageName);
+			        if (intent == null) {
+			            throw new NameNotFoundException(); 
+			        } 
+			        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+			        startActivity(intent);
+			    } catch (NameNotFoundException e) {
+			        Toast.makeText(
+			        		v.getContext(),
+			        		"The application "+packageName+" was not found! Possibly due to an exception: "+e.getMessage(),
+			        		Toast.LENGTH_LONG)
+			        .show(); 
+			    }
+			}
+		});
+
+		mImgBtnAppIsGood.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+		        Toast.makeText(
+		        		v.getContext(),
+		        		"Thanks for your feedback on app: "+packageName,
+		        		Toast.LENGTH_LONG)
+		        .show(); 
+			}
+		});
+
+		mImgBtnAppIsBad.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+		        Toast.makeText(
+		        		v.getContext(),
+		        		"Thanks for your feedback on app: "+packageName,
+		        		Toast.LENGTH_LONG)
+		        .show(); 
+			}
+		});
+	}
+
 	public List<PermissionInfo> getAppPermList() {
 		return appPermList;
 	}
